e40cba36d27c9ad6d25e63a99db2e56f622f3e75
Eswar Sai Ravi Chandu Sunkara <134662005+Eswarsaisunkara@users.noreply.github.com>
2026-04-27 14:12:21 +0530

    fix API endpoint

--- AI_Weed_Detection/Frontend/src/pages/api.js
+++ AI_Weed_Detection/Frontend/src/pages/api.js
@@ -1,8 +1,12 @@
-﻿export const predictImage = async (file) => {
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://ai-weed-detection.onrender.com";
+
+export const predictImage = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const res = await fetch("http://127.0.0.1:8000/predict", {
+  const res = await fetch(`${API_URL}/predict`, {
     method: "POST",
     body: formData,
   });
